ffee7d9eab8e90ac9260a34191a470f97dab9ef3
Esmeralda Nurce <EsmNur18@htl-shkoder.com>
2025-01-16 14:25:47 +0100

    calculator

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,29 @@
+public class Calculator {
+
+
+    public int add(int a, int b){
+        return a + b;
+    }
+
+    public int sub (int a, int b){
+        return a-b;
+    }
+
+    public int mult (int a, int b){
+        return a*b;
+    }
+
+    public int div (int a, int b){
+        if(b == 0){
+            throw new IllegalArgumentException();
+        }
+        return a/b;
+    }
+        }
+
+
+
+
+
+
+
